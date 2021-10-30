d94797b5a6577a137090c836150ad339909c26ec
Conor Ward <sirconorjamesward@gmail.com>
2021-10-30 11:45:50 -0600

    carousel!

--- horizontal-selections-and-carousels/src/App.js
+++ horizontal-selections-and-carousels/src/App.js
@@ -1,13 +1,29 @@
 import { HorizontalSelection } from "./HorizontalSelection";
+import { SimpleCarousel } from "./SimpleCarousel";
 const App = () => {
-  const test = ['one', 'two', 'three']
+
+  const range = (end) => {
+    let output =  []
+    for(let i = 0; i <= end; i++){
+      output.push(i)
+    }
+    return output
+  }
+
+  const test = range(100)
   return(
-    <HorizontalSelection
-    titles={test}
-    sections={test}
-    currentColor='blue-200'
-    normalColor='blue-600'
-    highlightColor='purple-400'/>
+    <>
+      <HorizontalSelection
+      titles={test}
+      sections={test}
+      currentColor='blue-200'
+      normalColor='blue-600'
+      highlightColor='purple-400'/>
+      <div className="h-64"></div>
+      <SimpleCarousel
+      sections={test}
+      renderDots={true}/>
+    </>
   )
 }
 
--- horizontal-selections-and-carousels/src/SimpleCarousel.js
+++ horizontal-selections-and-carousels/src/SimpleCarousel.js
@@ -1,3 +1,97 @@
-export const SimpleCarousel = ({sections}) => {
+import {useState, useEffect, useRef} from 'react'
 
+export const SimpleCarousel = ({sections, renderDots = false, dotSize = 7, dotFill, dotDuration = 300, touchSensativity = 100}) => {
+
+  const handleDotAnimation = (on, off) => {
+    on.classList.replace('scale-100', 'scale-150')
+    on.classList.replace('-translate-y-0', '-translate-y-2')
+    if(off){
+      off.classList.replace('scale-150','scale-100')
+      off.classList.replace('-translate-y-2', '-translate-y-0')
+    }
+  }
+
+  const testWindowSize = () => window.innerWidth > 640
+
+  const [windowTestResult, setWindowTestResult] = useState(testWindowSize)
+
+  useEffect(()=>{
+    window.addEventListener('resize', () => {
+      setWindowTestResult(testWindowSize)
+    })
+  },[])
+
+  const sectionsRef = useRef([])
+
+  const [current, setCurrent] = useState(null)
+
+  const handleCurrent = (newCurrent) => {
+    let update = null
+    if(newCurrent < 0){
+      update = sections.length -1
+    } else if(newCurrent > sections.length - 1){
+      update = 0
+    }else {
+      update = newCurrent
+    }
+    handleDotAnimation(sectionsRef.current[update].dot, sectionsRef.current[current].dot)
+    setCurrent(update)
+  }
+
+  const [startTouch, setStartTouch] = useState(null)
+
+  const handleTouch = (e) => {
+    if(!startTouch){
+      setStartTouch(e.changedTouches[0].screenX)
+    } else {
+      const screenX = e.changedTouches[0].screenX
+      if(Math.abs(screenX - startTouch) > touchSensativity){
+        if(screenX < startTouch){
+          handleCurrent(current + 1)
+        } else {
+          handleCurrent(current - 1)
+        }
+      }
+      setStartTouch(null)
+    }
+  }
+
+  useEffect(()=>{
+    setCurrent(0)
+    handleDotAnimation(sectionsRef.current[0].dot)
+  },[])
+
+  return (
+
+      <div className="bg-red-400">
+        <div className="flex justify-between">
+          {windowTestResult && <button id="previous" onClick={() => {handleCurrent(current - 1)}}
+          className="invisible md:visible text-8xl transition duration-300 transform hover:-translate-x-2 mx-3 z-30 mt-1/2">
+            {`<`}
+          </button>}
+          <div onTouchStart={handleTouch} onTouchEnd={handleTouch}>
+            {Boolean(current !== null) && sectionsRef.current[current].section}
+          </div>
+          {windowTestResult && <button id="next" onClick={()=>handleCurrent(current + 1)}
+          className="invisible md:visible text-8xl transition duration-300 transform hover:translate-x-2 mx-3 z-30">
+            {'>'}
+          </button>}
+        </div>
+        <div className="flex justify-center">
+          {renderDots && sections.map((section, i) => 
+            <button key={`dot-${i}`} onClick={() => handleCurrent(i)}
+            ref={dot => sectionsRef.current.push({dot:dot, section:section})}
+            className={`inline-block p-2 m-2 cursor-pointer transition duration-${dotDuration} scale-100 -translate-y-0 transform hover:scale-150 hover:-translate-y-2`}>
+            <svg height={`${dotSize}px`} width={`${dotSize}px`}>
+              <circle
+              cx={dotSize / 2}
+              cy={dotSize / 2}
+              r={dotSize / 2}
+              fill={dotFill}
+              />
+            </svg>
+          </button>)}
+        </div>
+      </div>
+  )
 }
